d810df826be7cba19128a15893233a3ff86530be
Kelly Stoops <kstoops815@gmail.com>
2018-01-29 20:29:33 -0600

    list of letter l fruits working

--- LinqedList/Program.cs
+++ LinqedList/Program.cs
@@ -10,6 +10,19 @@ namespace LinqedList
     {
         static void Main(string[] args)
         {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            var LFruits = from fruit in fruits
+                          where fruit.StartsWith("L")
+                          select fruit;
+
+            foreach (var LFruit in LFruits)
+            {
+                Console.WriteLine(LFruit);
+            }
+
+            Console.ReadLine();
         }
     }
 }
