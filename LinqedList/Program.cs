9bd7684a6d501ab17cf4be8c6817807fa6a467d3
Kelly Stoops <kstoops815@gmail.com>
2018-01-29 20:40:27 -0600

    list of numbers that are multiples of 4 or 6 done

--- LinqedList/Program.cs
+++ LinqedList/Program.cs
@@ -22,6 +22,21 @@ namespace LinqedList
                 Console.WriteLine(LFruit);
             }
 
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0);
+
+            foreach (var multiple in fourSixMultiples)
+            {
+                Console.WriteLine($"The number {multiple} is a multiple of either 4 and/or 6.");
+            }
+
+
+
             Console.ReadLine();
         }
     }
